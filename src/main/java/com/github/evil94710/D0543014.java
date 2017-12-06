3aa077a65d3a2d5b560fc52b6121c1109ddf1409
evil94710 <evil94710@gmail.com>
2017-12-06 12:07:18 +0800

    create add function

---
+++ src/main/java/com/github/evil94710/D0543014.java
@@ -0,0 +1,13 @@
+package com.github.evil94710;
+
+public class D0543014 {
+    public int add(int a ,int b){
+        int num = a+b;
+        if(num < 10){
+            return num;
+        }
+        else{
+            return 0;
+        }
+    }
+}
---
+++ src/test/java/com/github/evil94710/D0543014Test.java
@@ -0,0 +1,17 @@
+package com.github.evil94710;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class D0543014Test {
+    @Test
+    public void add() throws Exception {
+        D0543014 newadd = new D0543014();
+
+        int ans = newadd.add(2, 3);
+
+        assertEquals(5,ans);
+    }
+
+}

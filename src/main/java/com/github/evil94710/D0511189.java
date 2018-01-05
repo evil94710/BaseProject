11b4a3efe222e6615b0201cb7963cc750172d977
mini <alysa8612276161911@gmail.com>
2018-01-05 17:05:53 +0800

    UPLOAD SEARCH

--- src/main/java/com/github/evil94710/D0511189.java
+++ src/main/java/com/github/evil94710/D0511189.java
@@ -3,7 +3,7 @@ package com.github.evil94710;
 import java.util.Scanner;
 public class D0511189 {
     public static void main(String[] args) {
-        int i=0,j=0,k=0,s,p,Array[20];
+        int i=0,j=0,k=0,s,p,count,Array[20];
         Scanner keyboard = new Scanner(System.in);
         System.out.println("1.午餐"+"\n"+"2.晚餐"+"\n"+"3.點心"+"\n"+"4.宵夜"+"\n");
         System.out.println("input your condition number(only one):"+"\n");
@@ -17,7 +17,7 @@ public class D0511189 {
         else{
             switch(condition){
                 case 1:
-                    for(p=0;p<20;p++){
+                    for(p=0;p<count;p++){
                         if(list[s][5]==1){
                         list[p][0]=Array[i];
                         i++;
@@ -25,7 +25,7 @@ public class D0511189 {
                     }
                     break;
                 case 2:
-                    for(p=0;p<20;p++) {
+                    for(p=0;p<count;p++) {
                         if (list[s][6] == 1) {
                             list[p][0] = Array[i];
                             i++;
@@ -33,7 +33,7 @@ public class D0511189 {
                     }
                     break;
                 case 3:
-                    for(p=0;p<20;p++) {
+                    for(p=0;p<count;p++) {
                         if (list[s][7] == 1) {
                             list[p][0] = Array[i];
                             i++;
@@ -41,7 +41,7 @@ public class D0511189 {
                     }
                     break;
                 case 4:
-                    for(p=0;p<20;p++) {
+                    for(p=0;p<count;p++) {
                         if (list[s][8] == 1) {
                             list[p][0] = Array[i];
                             i++;

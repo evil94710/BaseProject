711c3da8d4d918f5bd22afd772f88e24a65385e2
mini <alysa8612276161911@gmail.com>
2018-01-15 23:35:06 +0800

    Merge branch 'master' of https://github.com/evil94710/BaseProject
    # Conflicts:
    #	src/main/java/com/github/evil94710/D0588491.java
    #	src/main/java/com/github/evil94710/Main.java

--- src/main/java/com/github/evil94710/D0511189.java
+++ src/main/java/com/github/evil94710/D0511189.java
@@ -1,30 +1,29 @@
 package com.github.evil94710;
-
+import java.util.Random;
 import java.util.Scanner;
 public class D0511189 {
     private int condition;
     Scanner keyboard = new Scanner(System.in);
-    public void setFunction(){
-        System.out.println("1.午餐"+"\n"+"2.晚餐"+"\n"+"3.宵夜"+"\n"+"4.點心"+"\n");
-        System.out.println("input your condition number(only one and number only):"+"\n");
+
+    public void setFunction() {
+        System.out.println("1.午餐" + "\n" + "2.晚餐" + "\n" + "3.宵夜" + "\n" + "4.點心" + "\n");
+        System.out.println("input your condition number(only one and number only):" + "\n");
         condition = keyboard.nextInt();
     }
 
-    public void determine(String[] arrName,String[] arrClass,int count){
+    public void determine(String[] arrName, String[] arrClass, int count) {
         int i = 0, k = 0, j, p;
-        String[]  arr = new String[200];
-        String Condition= String.valueOf(condition);
-        if(condition < 1 || condition > 4){
-            System.out.print("輸入錯誤"+"\n");
-        }
-        else if(Condition.equals("")){
-            System.out.print("輸入不能為空白哦"+"\n");
-        }
-        else {
+        String[] arr = new String[200];
+        String Condition = String.valueOf(condition);
+        if (condition < 1 || condition > 4) {
+            System.out.print("輸入錯誤" + "\n");
+        } else if (Condition.equals("")) {
+            System.out.print("輸入不能為空白哦" + "\n");
+        } else {
             switch (condition) {
                 case 1:
                     for (p = 0; p < count; p++) {
-                        if (arrClass[p].charAt(0)=='1') {
+                        if (arrClass[p].charAt(0) == '1') {
                             arr[i] = arrName[p];
                             i++;
                         }
@@ -55,11 +54,13 @@ public class D0511189 {
                     }
                     break;
             }
-            for(j = 0;j < arr.length;j ++){
-                System.out.println(arr[j]);
-                k++;
-                if( k == 5 )
-                    break;
+            for (j = 0; j < 5; j++) {
+                Random ran = new Random();
+                int num = ran.nextInt(i);
+                System.out.println(arr[num]);
+                /*k++;
+                if (k == 5)
+                    break;*/
             }
         }
     }
